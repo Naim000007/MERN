25d496039ef7b80256afaf5ec658d9732e11c308
Naim000007 <mdnaim01303202218@gmail.com>
2024-05-12 08:15:08 +0600

    placeorder left side html complete

--- Food-delivary/src/page/cart/Cart.jsx
+++ Food-delivary/src/page/cart/Cart.jsx
@@ -2,9 +2,11 @@ import React, { useContext } from 'react'
 import "./Cart.css"
 import { StoreContext } from '../../Context/StoreContex'
 import { assets } from '../../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext)
+  const navigate = useNavigate()
   return (
     <div className='cart'>
       <div className='cart-items'>
@@ -62,7 +64,7 @@ const Cart = () => {
               {/* <hr /> */}
             </div>
           </div>
-          <button>Checkout</button>
+          <button onClick={() => navigate("/order")}>Checkout</button>
         </div>
         <div className='cart-promocode'>
           <div>
--- Food-delivary/src/page/placeorder/PlaceOrder.jsx
+++ Food-delivary/src/page/placeorder/PlaceOrder.jsx
@@ -3,7 +3,27 @@ import './PlaceOrder.css'
 
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
+    <div className='place-order'>
+      <div className='place-order-left'>
+        <p className='title'>Delivery Information</p>
+        <div className='multi-fields'>
+          <input type='text' placeholder='First Name' />
+          <input type='text' placeholder='Last Name' />
+        </div>
+        <input type='Email' placeholder='Email Address' />
+        <input type='text' placeholder='Street' />
+        <div className='multi-fields'>
+          <input type='text' placeholder='City' />
+          <input type='text' placeholder='Division' />
+          <div className='multi-fields'>
+            <input type='text' placeholder='Zip Code' />
+            <input type='text' placeholder='Country' />
+          </div>
+          <input type='text' placeholder='Phone Number' />
+        </div>
+      </div>
+      <div className='place-order-right'></div>
+    </div>
   )
 }
 
