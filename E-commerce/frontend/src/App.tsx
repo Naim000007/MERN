3bd2065172301af7f913206c626a056b08a791f7
Naim000007 <mdnaim01303202218@gmail.com>
2024-05-28 21:10:42 +0600

    another bug fixed

--- E-commerce/frontend/src/App.tsx
+++ E-commerce/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { lazy, Suspense } from "react"
-// import Loader from "./components/admin/Loader"
+import Loader from "./Components/admin/Loader"
 
 const Home = lazy(() => import("./pages/Home"))
 const Cart = lazy(() => import("./pages/Cart"))
@@ -31,43 +31,43 @@ const App = () => {
 
   return (
     <Router>
-      {/* <Suspense fallback={<Loader />}> */}
+      <Suspense fallback={<Loader />}>
 
-      {/* Header */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/cart" element={<Cart />} />
+        {/* Header */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
 
 
-        {/* admin  */}
-        <Route
-        // element={
-        //   <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
-        // }
-        >
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/product" element={<Products />} />
-          <Route path="/admin/customer" element={<Customers />} />
-          <Route path="/admin/transaction" element={<Transaction />} />
-          {/* Charts */}
-          <Route path="/admin/chart/bar" element={<Barcharts />} />
-          <Route path="/admin/chart/pie" element={<Piecharts />} />
-          <Route path="/admin/chart/line" element={<Linecharts />} />
-          {/* Apps */}
-          <Route path="/admin/app/coupon" element={<Coupon />} />
-          <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
-          <Route path="/admin/app/toss" element={<Toss />} />
+          {/* admin  */}
+          <Route
+          // element={
+          //   <ProtectedRoute isAuthenticated={true} adminRoute={true} isAdmin={true} />
+          // }
+          >
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/product" element={<Products />} />
+            <Route path="/admin/customer" element={<Customers />} />
+            <Route path="/admin/transaction" element={<Transaction />} />
+            {/* Charts */}
+            <Route path="/admin/chart/bar" element={<Barcharts />} />
+            <Route path="/admin/chart/pie" element={<Piecharts />} />
+            <Route path="/admin/chart/line" element={<Linecharts />} />
+            {/* Apps */}
+            <Route path="/admin/app/coupon" element={<Coupon />} />
+            <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+            <Route path="/admin/app/toss" element={<Toss />} />
 
-          {/* Management */}
-          <Route path="/admin/product/new" element={<NewProduct />} />
+            {/* Management */}
+            <Route path="/admin/product/new" element={<NewProduct />} />
 
-          <Route path="/admin/product/:id" element={<ProductManagement />} />
+            <Route path="/admin/product/:id" element={<ProductManagement />} />
 
-          <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
-        </Route>;
-      </Routes>
-      {/* </Suspense> */}
+            <Route path="/admin/transaction/:id" element={<TransactionManagement />} />
+          </Route>;
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
